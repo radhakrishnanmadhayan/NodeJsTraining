e0b7afae49bfd373c544191949a5823ace8046b2
radhakrishnanmadhayan <123364297+radhakrishnanmadhayan@users.noreply.github.com>
2023-01-25 15:08:03 +0530

    Update day_two_assignments.js

--- day_two_assignments.js
+++ day_two_assignments.js
@@ -11,13 +11,12 @@ function squareOfAnArray(arr)
 
 function getHighestSalaryEmp(arrOfObj)
 {
-    var pos;
-    //var resObj;
+    var pos=0;
     for(let i=0;i<(arrOfObj.length-1);i++)
     {
-        if (arrOfObj[i]["salary"] < arrOfObj[i+1]["salary"] )
+        if (arrOfObj[i]["salary"] > arrOfObj[pos]["salary"] )
         {
-            pos=i+1;
+            pos=i;
         }
     }    
     return arrOfObj[pos];;
@@ -37,16 +36,15 @@ function createMaskOnPhoneNumber(phoneNumber)
         }
     }
     return maskedphNo;
-
 }
 
 
 
 var arr=[10,20,30,40,50];
-var empArr=[{empId:101,empName:"Asha",salary:1001,deptId:"D1"},
+var empArr=[{empId:101,empName:"Asha",salary:100100,deptId:"D1"},
             {empId:102,empName:"Gaurav",salary:2000,deptId:"D1"},
             {empId:103,empName:"Karan",salary:2000,deptId:"D2"},
-            {empId:104,empName:"Kishan",salary:3000,deptId:"D1"},
+            {empId:104,empName:"Kishan",salary:100100,deptId:"D1"},
             {empId:105,empName:"Keshav",salary:3500,deptId:"D2"},
             {empId:106,empName:"Pran",salary:4000,deptId:"D3"},
             {empId:107,empName:"Saurav",salary:3800,deptId:"D3"}]
